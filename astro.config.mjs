0262cd8bb7c752021b0883d1533e4a240bddfb81
Matthew Revell <matthew@hoopy.io>
2025-01-22 10:09:57 +0000

    Added astro-paraglide and moved translation files to the appropriate location

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,22 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    paraglide({
+      // recommended settings
+      project: "./project.inlang",
+      outdir: "./src/paraglide", //where your files should be
+    }),
+  ],
   i18n: {
     defaultLocale: "en", // The default language used as a fallback
     locales: ["en", "es", "fr"], // All supported languages on the site
     routing: {
       prefixDefaultLocale: true, // URL structure will be:
-                                  // / -> English (default)
-                                  // /es -> Spanish
-                                  // /fr -> French
+      // / -> English (default)
+      // /es -> Spanish
+      // /fr -> French
     },
   },
 });
